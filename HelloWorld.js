e36930b08754a6df10b52b64f47b8f2fd7f946cb
Nitesh Tak <niksrj.tak@gmail.com>
2021-01-06 10:23:15 +0530

    HTTP MODULE

---
+++ HelloWorld.js
@@ -0,0 +1,12 @@
+var http =require("http");
+var url= require("url");
+var dt= require('./MyFirstModule')
+
+http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-type':'text/html'});
+    res.write(req.url+"<br/>");
+    var q= url.parse(req.url,true).query;
+    res.write(q.year+'&nbsp &nbsp   '+q.month+"<br/>")   //http://localhost:3000/nitesh/?year=2021&month=July
+    res.write('Hello World +'+dt.myDateTime());
+    res.end();
+}).listen(3000);
